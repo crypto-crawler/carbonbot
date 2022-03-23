c039c5401d3d7f2e624aa3cdc412443587b3a90a
soulmachine <soulmachine@gmail.com>
2022-03-23 19:30:06 +0000

    Moved from crypto-crawler/crypto-crawler-rs/tree/main/carbonbot

---
+++ conf/run_crawlers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+LOCAL_DATA_DIR="/your/local/path"       # required
+AWS_ACCESS_KEY_ID="you access key"      # required
+AWS_SECRET_ACCESS_KEY="your secret key" # required
+AWS_S3_DIR="s3://bucket/your/s3/path"   # required
+
+docker pull ghcr.io/crypto-crawler/carbonbot:latest
+
+mkdir -p $LOCAL_DATA_DIR
+
+# l2_snapshot and open_interest are not included, better deploy them in a different network
+msg_types=("trade" "l2_event" "l2_topk" "l3_event" "bbo" "ticker" "candlestick" "funding_rate" "other")
+
+for msg_type in ${msg_types[@]}; do
+  docker stop carbonbot-$msg_type && docker rm carbonbot-$msg_type
+  docker run -d --name carbonbot-$msg_type --restart always -v $LOCAL_DATA_DIR:/data -e DATA_DIR=/data -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_S3_DIR=$AWS_S3_DIR -u "$(id -u):$(id -g)" ghcr.io/crypto-crawler/carbonbot:latest pm2-runtime start pm2.$msg_type.config.js
+done
+
+docker system prune -af
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub(crate) mod misc_crawlers;
+pub(crate) mod writers;
+
+pub use misc_crawlers::crawl_other;
+pub use writers::create_writer_threads;
