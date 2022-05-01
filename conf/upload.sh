faaa9614429089783f52773cf5178f8f2521e760
soulmachine <soulmachine@gmail.com>
2022-05-01 06:18:12 +0000

    Use move is only one of DEST_DIR, AWS_S3_DIR or MINIO_DIR is set

--- conf/upload.sh
+++ conf/upload.sh
@@ -16,11 +16,14 @@ if [[ -z "${DEST_DIR}" && -z "${AWS_S3_DIR}" && -z "${MINIO_DIR}" && -z "${REDIS
   exit 1
 fi
 
+num_destinations=0
+
 if [[ -n "${AWS_S3_DIR}" ]]; then
   if [[ -z "${AWS_ACCESS_KEY_ID}"  ||  -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
     echo "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be set" >&2
   exit 1
   fi
+  num_destinations=$((num_destinations+1))
 fi
 
 if [[ -n "${MINIO_DIR}" ]]; then
@@ -28,10 +31,19 @@ if [[ -n "${MINIO_DIR}" ]]; then
     echo "AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and MINIO_ENDPOINT_URL must be set" >&2
   exit 1
   fi
+  num_destinations=$((num_destinations+1))
 fi
 
 if [[ -n "${DEST_DIR}" ]]; then
   mkdir -p "$DEST_DIR/$msg_type"
+  num_destinations=$((num_destinations+1))
+fi
+
+# optional optimization, if only one of DEST_DIR, AWS_S3_DIR or MINIO_DIR is set, then is_move is true
+if [ $num_destinations -gt 1 ]; then
+  sub_command="copy"
+else
+  sub_command="move"
 fi
 
 # Infinite while loop
@@ -41,25 +53,22 @@ do
   find "$DATA_DIR/$msg_type" -name "*.json" -type f -mmin +1 | xargs -r -n 1 pigz -f
   success=true
   if [[ -n "${AWS_S3_DIR}" ]]; then
-    rclone --s3-region "${AWS_REGION:-us-east-1}" --immutable --contimeout=1s --retries 1 --low-level-retries 1 copy "$DATA_DIR/$msg_type" "$AWS_S3_DIR/$msg_type" --include '*.json.gz' --no-traverse --transfers=8
-    if [ $? -ne 0 ]; then
+    if ! rclone --s3-region "${AWS_REGION:-us-east-1}" --immutable --contimeout=1s --retries 1 --low-level-retries 1 $sub_command "$DATA_DIR/$msg_type" "$AWS_S3_DIR/$msg_type" --include '*.json.gz' --no-traverse --transfers=8; then
       success=false
     fi
   fi
   if [[ -n "${MINIO_DIR}" ]]; then
-    rclone --s3-region "${AWS_REGION:-us-east-1}" --s3-endpoint $MINIO_ENDPOINT_URL --immutable --contimeout=1s --retries 1 --low-level-retries 1 copy "$DATA_DIR/$msg_type" "$MINIO_DIR/$msg_type" --include '*.json.gz' --no-traverse --transfers=8
-    if [ $? -ne 0 ]; then
+    if ! rclone --s3-region "${AWS_REGION:-us-east-1}" --s3-endpoint "$MINIO_ENDPOINT_URL" --immutable --contimeout=1s --retries 1 --low-level-retries 1 $sub_command "$DATA_DIR/$msg_type" "$MINIO_DIR/$msg_type" --include '*.json.gz' --no-traverse --transfers=8; then
       success=false
     fi
   fi
   if [[ -n "${DEST_DIR}" ]]; then
-    rclone move "$DATA_DIR/$msg_type" "$DEST_DIR/$msg_type" --include '*.json.gz' --no-traverse --transfers=8
-    if [ $? -ne 0 ]; then
+    if ! rclone $sub_command "$DATA_DIR/$msg_type" "$DEST_DIR/$msg_type" --include '*.json.gz' --no-traverse --transfers=8; then
       success=false
     fi
   fi
 
-  if [ "$success" = true ]; then
+  if [[ "$success" = true && "$sub_command" = "copy" ]]; then
     rclone delete "$DATA_DIR/$msg_type" --include '*.json.gz'
   fi
 
